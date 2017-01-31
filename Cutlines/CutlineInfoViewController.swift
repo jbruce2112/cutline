9b333a4f1be387f4458e084ebe0ec846a8b497a7
John Bruce <jbruce2112@gmail.com>
2017-01-30 22:57:08 -0500

    initial commit - storyboard layout

---
+++ Cutlines/CutlineInfoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CutlineInfoViewController.swift
+//  Cutlines
+//
+//  Created by John Bruce on 1/30/17.
+//  Copyright © 2017 Bruce32. All rights reserved.
+//
+
+import UIKit
+
+class CutlineInfoViewController: UIViewController {
+	
+}
---
+++ Cutlines/CutlinesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CutlinesViewController.swift
+//  Cutlines
+//
+//  Created by John Bruce on 1/30/17.
+//  Copyright © 2017 Bruce32. All rights reserved.
+//
+
+import UIKit
+
+class CutlinesViewController: UIViewController {
+	
+}
