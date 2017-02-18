7de4d911bad685669e4500983d8cb1987c85a025
John Bruce <jbruce2112@gmail.com>
2017-02-18 15:10:38 -0500

    Clean up the settings page. Add a couple new bar button icons for Settings.

--- Cutlines/SettingsViewController.swift
+++ Cutlines/SettingsViewController.swift
@@ -11,17 +11,36 @@ import UIKit
 class SettingsViewController: UIViewController {
 	
 	@IBOutlet var versionLabel: UILabel!
-	@IBOutlet var attributionLabel: UILabel!
+	@IBOutlet var attributionTextView: UITextView!
+	
+	let version: String = {
+		
+		guard
+			let infoDict = Bundle.main.infoDictionary,
+			let build = infoDict[kCFBundleVersionKey as String] as? String,
+			let version = infoDict["CFBundleShortVersionString"] as? String else {
+				return "0.0.0"
+		}
+		
+		return "Version \(version).\(build)"
+	}()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		guard let version = Bundle.main.infoDictionary?[kCFBundleVersionKey as String] as? String else {
-			return
-		}
+		versionLabel.text = version
 		
-		versionLabel.text = "Version \(version)"
-		attributionLabel.text = "Icons pack by Icons8: https://icons8.com"
+		let linkTitle = "Icons8"
+		let text = "Icon pack by \(linkTitle)" as NSString
+		let linkTitleRange = text.range(of: linkTitle)
+		
+		// Create an AttributedString with the Icons8 URL with the standard system font
+		let attribution = NSMutableAttributedString(string: text as String)
+		attribution.addAttribute(NSLinkAttributeName, value: "https://icons8.com", range: linkTitleRange)
+		attribution.addAttribute(NSFontAttributeName, value: UIFont.systemFont(ofSize: 17.0), range: NSMakeRange(0, text.length))
+		attributionTextView.attributedText = attribution
+		
+		// Align the leading edge with the versionLabel
+		attributionTextView.textContainer.lineFragmentPadding = 0
 	}
-	
 }
