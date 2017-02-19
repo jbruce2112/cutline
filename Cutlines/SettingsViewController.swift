9f13d194a7ccd296e4d47c8bca0f993b0ad5b4a0
John Bruce <jbruce2112@gmail.com>
2017-02-18 23:56:15 -0500

    Make the settings scene a table view

--- Cutlines/SettingsViewController.swift
+++ Cutlines/SettingsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UITableViewController {
 	
 	@IBOutlet var versionLabel: UILabel!
 	@IBOutlet var attributionTextView: UITextView!
