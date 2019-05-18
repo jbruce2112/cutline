d31b9320d4a7a195b5e23c6abefb523a5ddfe8f9
John Bruce <jbruce2112@gmail.com>
2019-05-18 13:29:36 -0700

    auto generate app store snapshots with fastlane

--- CutlinesUITests/CutlinesUITests.swift
+++ CutlinesUITests/CutlinesUITests.swift
@@ -1,33 +1,79 @@
-//
-//  CutlinesUITests.swift
-//  CutlinesUITests
-//
-//  Created by John on 5/18/19.
-//  Copyright © 2019 Bruce32. All rights reserved.
-//
-
 import XCTest
 
 class CutlinesUITests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        let app = XCUIApplication()
-        setupSnapshot(app)
-        app.launch()
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
+	
+	func testCreateGridScreenshot() {
+		let app = XCUIApplication()
+		setupSnapshot(app)
+		app.launch()
+		snapshot("1_GridScreen")
+	}
+	
+	func testCreateShareScreenshot() {
+		let app = XCUIApplication(bundleIdentifier: "com.apple.mobileslideshow")
+		setupSnapshot(app)
+		app.launch()
+		
+		let milesWatchingBirdVideoPhoto = app.cells.element(boundBy: 7)
+		milesWatchingBirdVideoPhoto.tap()
+		
+		app.toolbars.matching(identifier: "Toolbar").toolbars.matching(identifier: "Toolbar").buttons["Share"].tap()
+		
+		let shareExtensionButton = app.buttons["Add Caption"]
+		if !shareExtensionButton.waitForExistence(timeout: 5.0) {
+			let moreButton = app.buttons["More"]
+			moreButton.tap()
+			
+			let shareExtensionSwitch = app.switches["Add Caption"]
+			_ = shareExtensionSwitch.waitForExistence(timeout: 5.0)
+			
+			if shareExtensionSwitch.value as? String != "1" {
+				shareExtensionSwitch.tap()
+				print("tapped")
+			}
+			
+			let doneButton = app.buttons["Done"]
+			_ = doneButton.waitForExistence(timeout: 5.0)
+			doneButton.tap()
+		}
+		
+		_ = shareExtensionButton.waitForExistence(timeout: 5.0)
+		shareExtensionButton.tap()
+		
+		let postModal = app.buttons["Post"]
+		_ = postModal.waitForExistence(timeout: 5.0)
+		
+		snapshot("2_ShareExtensionScreen")
+	}
+	
+	func testCreateCaptionSearchScreenshot() {
+		
+		let app = XCUIApplication()
+		setupSnapshot(app)
+		app.launch()
+		
+		app.tabBars.buttons["Search"].tap()
+		let searchField = app.searchFields["Search"]
+		searchField.tap()
+		
+		searchField.typeText("mackinac")
+		
+		snapshot("3_CaptionSearchScreen")
+	}
+	
+	func testCreateShareMemoriesScreenshot() {
+		
+		let app = XCUIApplication()
+		setupSnapshot(app)
+		app.launch()
+		
+		let mackinacSunsetPhoto = app.collectionViews.cells.element(boundBy: 17)
+		mackinacSunsetPhoto.tap()
+		app.navigationBars["Edit"].buttons["refresh"].tap()
+		
+		let editHeader = app.staticTexts["Edit"]
+		_ = editHeader.waitForExistence(timeout: 5.0)
+		
+		snapshot("4_ShareMemories")
+	}
 }
